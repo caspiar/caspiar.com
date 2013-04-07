9810366572e37b663a78a55594f57b0dd4237cb4
Malcolm Diggs <malcolm@malcolmdiggs.com>
2013-04-07 02:34:10 -0700

    More copy changes

--- includes/all_page_content.php
+++ includes/all_page_content.php
@@ -12,17 +12,17 @@
 $all_pages = array(
   'home' => array(
     'url' => 'home',
-    'title' => 'A full service digital marketing agency',
+    'title' => 'Winning feels awesome. We&#39;ll get you there.',
     'blurb' => '<a class="call_to_action" href="about">We&#39;ll get you there.</a>',
     'headline' => '<span class="white">Winning<br />Feels<br />Awesome.</span>',
-    'meta_description' => 'Welcome to Caspiar, a full-service digital marketing agency in San Francisco. We specialize in web-development, app development, and campaign management. Contact us today to start a conversation.'
+    'meta_description' => 'Caspiar is a full-service digital marketing agency in San Francisco. We believe in agile software development, responsive campaign management, and transparent customer service.'
     ),
   'about' => array(
     'url' => 'about',
     'title' => 'A startup&#39;s best friend',
     'blurb' => '<p>Agile development &<br />marketing services <br />to fit any timeline<br />and any budget.</p><a class="call_to_action" href="contact">Bring us your ideas.</a>',
     'headline' => 'A startup&#39;s<br />best friend',
-    'meta_description' => 'We offer agile development and marketing services for startups and small businesses. Digital solutions to fit any timeline, any budget, and any vertical.'
+    'meta_description' => 'We offer agile development and marketing services for startups and small businesses. Digital solutions to fit any timeline, any budget, and any vertical. Bring us your ideas.'
     ),
   'contact' => array(
     'url' => 'contact',
@@ -34,8 +34,8 @@ $all_pages = array(
   'careers' => array(
     'url' => 'careers',
     'title' => 'Join us for a sprint',
-    'blurb' => '<p>Got talent? Tweet at us:</p><a class="call_to_action" href="http://twitter.com/team_caspiar" target="_blank" rel="nofollow">@team_caspiar</a><p>If you can&#39;t explain why <br />you&#39;re awesome in less<br />than 140 characters,<br />you don&#39;t belong in<br />marketing.</p>',
-    'headline' => 'Sprint with Us',
+    'blurb' => '<p>Are you the next...<br />...Don Draper?<br />...Linus Torvalds?<br />...Andy Warhol?</p><p>Send us a tweet</p><a class="call_to_action" href="http://twitter.com/team_caspiar" target="_blank" rel="nofollow">@team_caspiar</a><p>Tell us what you do and<br />why you&#39;re awesome.</p>',
+    'headline' => '"Sprint" with Us',
     'meta_description' => 'Caspiar is now hiring talented engineers, creative marketers, aggressive sales people, and aspiring interns. Contact us for more information.'
     ),
   'login' => array(
